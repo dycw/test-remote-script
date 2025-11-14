ee672f591a44693d85ef912be877fe84401dbb6a
Derek Wan <d.wan@icloud.com>
2025-11-14 20:47:38 +0900

    2025-11-14 20:47:38 (Fri)  > DW-Mac  > derekwan

--- src/test_remote_script/main.py
+++ src/test_remote_script/main.py
@@ -8,18 +8,16 @@ from utilities.logging import basic_config
 from test_remote_script import __version__
 
 _LOGGER = getLogger(__name__)
-print(__name__)  # # noqa: T201
+basic_config(obj=_LOGGER, hostname=True)
 
 
 @command()
 @option("--asdf", is_flag=True, default=False, show_default=False)
 def _main(*, asdf: bool) -> None:
-    basic_config(obj=_LOGGER, hostname=True)
     _LOGGER.info("Running main %s...", __version__)
     _LOGGER.info("And....%s", asdf)
     _LOGGER.warning("Running main %s...", __version__)
 
 
 if __name__ == "__main__":
-    basic_config(obj=_LOGGER, hostname=True)
     _main()
