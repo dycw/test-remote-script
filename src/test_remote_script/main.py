d2fca31a4cdb6c23686a3dbeacabe58afd15e9df
Derek Wan <d.wan@icloud.com>
2025-11-14 20:39:11 +0900

    2025-11-14 20:39:11 (Fri)  > DW-Mac  > derekwan

--- src/test_remote_script/main.py
+++ src/test_remote_script/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from logging import getLogger
 
-from click import command
+from click import command, option
 from utilities.logging import basic_config
 
 from test_remote_script import __version__
@@ -11,9 +11,11 @@ _LOGGER = getLogger(__name__)
 
 
 @command()
-def _main() -> None:
+@option("--asdf", is_flag=True, default=False, show_default=False)
+def _main(*, asdf: bool) -> None:
     basic_config(obj=_LOGGER, hostname=True)
     _LOGGER.info("Running main %s...", __version__)
+    _LOGGER.info("And....%s", asdf)
     _LOGGER.warning("Running main %s...", __version__)
 
 
